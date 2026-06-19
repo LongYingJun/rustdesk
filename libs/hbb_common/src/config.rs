2bcf5afdaaa586d4638c8813c7223b6304ccbce1
龙英俊 <970882491@qq.com>
2026-06-19 13:13:51 +0800

    修改为10.1.2.254地址

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -120,8 +120,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.1.2.254"];
+pub const PUBLIC_RS_PUB_KEY: &str = "VYOwRQzrjY9v5GYP8FtSUkyuubhYMYARskQmOT0KFO4=";
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
